7223f249c9e8ad69aaed5c9be792722768f331e5
shivanisharma0710 <shivanibitto07@gmail.com>
2023-05-30 23:44:46 +0530

    add variable interchange without using 3rd variable

--- cd.c
+++ cd.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-  int c,d,e;
+  int c,d;
   printf("enter the number at location c:");
   scanf("%d",&c);
   printf("enter the number at loaction d:");
   scanf("%d",&d);
-  e=c;
-  c=d;
-  d=e;
+  c=c+d;
+  d=c-d;
+  c=c-d;
   printf("the number at location c=%d\n",c);
   printf("the number at location d=%d\n",d);
 }
