62ffa9153b0aec8043a77c1833cc4bf97d643f7d
shivanisharma0710 <shivanibitto07@gmail.com>
2023-06-02 00:10:56 +0530

    add program to check if a triangle is valid

---
+++ validtrianglee.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int side1,side2,side3,largeside,sum;
+    printf("enter three sides of triangle:");
+    scanf("%d%d%d", &side1,&side2,&side3);
+    if(side1>side2)
+    {
+        if(side1>side3)
+    {
+        sum=side2+side3;
+        largeside=side1;
+    }
+    else
+    {
+        sum=side1+side2;
+        largeside=side3;
+    }
+    }
+    else
+    {
+       if(side2>side3)
+       {
+        sum=side1+side3;
+        largeside=side2;
+       }
+       else
+       {
+         sum=side1+side2;
+         largeside=side3;
+       }
+    }
+    if(sum>largeside)
+    printf("the triangle is a valid\n");
+    else
+    printf("the triangle is invalid\n");
+}
