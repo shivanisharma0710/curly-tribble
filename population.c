b1d7c299b704454eda6c7942f66fa8f681a07cac
shivanisharma0710 <shivanibitto07@gmail.com>
2023-06-11 19:44:02 +0530

    add this program to specify population at the end of each year

---
+++ population.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    float pop=100000;
+    for(i=1;i<=10;i++)
+    {
+        pop = pop - pop*0.1; 
+        printf("%d year: %d\n",i, (int)pop);
+    }
+}
