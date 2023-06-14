93e9768d61b6604971fa9f14d5e8705d2b11a2d6
shivanisharma0710 <shivanibitto07@gmail.com>
2023-06-14 14:21:39 +0530

    add this program to determine whether number is prime or not using function

---
+++ primenofunc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int prime(int n);
+int main()
+{
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    prime(num);
+}
+int prime(int n)
+{
+    int a;
+    for(a=2;a<=n;a++)
+    {
+        if(n%a==0)
+        {
+            printf("%d, ",a);
+        }
+    }
+}
