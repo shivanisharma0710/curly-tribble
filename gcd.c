e792e88cd681a06e279ea293ddde5a14b2cafb73
shivanisharma0710 <shivanibitto07@gmail.com>
2023-06-15 17:52:50 +0530

    add this program to find out the greatest common divisor of number

---
+++ gcd.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int gcd(int a, int b);
+int main()
+{
+    int a,b;
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+    printf("Greatest Common Divisor of (%d, %d): %d",a,b,gcd(a,b));
+}
+int gcd(int a, int b)
+{
+    int t;
+    while(b != 0)
+    {
+    t = b;
+    b = a % b;
+    a = t;
+    }
+    return a;
+}
