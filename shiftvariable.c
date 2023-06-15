958770b8feba2363c02fd6daa2ae75768b11ef8c
shivanisharma0710 <shivanibitto07@gmail.com>
2023-06-15 17:53:27 +0530

    add this program to shift value of one variable to another

---
+++ shiftvariable.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int shift(int *a, int *b, int *c);
+int main()
+{
+    int x, y, z;
+    printf("Enter x: ");
+    scanf("%d", &x);
+    printf("Enter y: ");
+    scanf("%d", &y);
+    printf("Enter z: ");
+    scanf("%d", &z);
+    printf("Before Shift: x: %d\ty: %d\tz: %d",x,y,z);
+    shift(&x, &y, &z);
+    printf("\nAfter Shift: x: %d\ty: %d\tz: %d",x,y,z);
+}
+int shift(int *a, int *b, int *c)
+{
+    int temp;
+    temp = *c;
+    *c = *b;
+    *b = *a;
+    *a = temp;  
+}
